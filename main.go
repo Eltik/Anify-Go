ef33528c05a5442968e172b1d7be146788a6aefe
Eltik <xp10d363@gmail.com>
2024-11-06 12:07:15 -0800

    feat: Finished MangaDex base

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	proxies "anify/eltik/go/src/lib/impl/proxies"
 	"anify/eltik/go/src/lib/impl/request"
 	"anify/eltik/go/src/mappings/impl/base"
-	"anify/eltik/go/src/types"
 )
 
 func main() {
@@ -21,15 +20,64 @@ func main() {
 		println(*proxy)
 	}
 
-	res, err := base.NewMangaDexBaseProvider().SearchAdvanced("", types.TypeManga, []types.Format{types.FormatManga}, 0, 25, []string{"Aliens"}, []string{"Harem"}, types.SeasonUnknown, 0, []string{"Isekai"}, []string{"Harem"})
+	res, err := base.NewMangaDexBaseProvider().GetIds()
 	if err != nil {
 		panic(err)
 	}
 
-	for _, r := range res {
-		// print r.title.english
-		println(*r.Title.English)
-	}
+	println(len(res))
+
+	/*
+		res, err := base.NewMangaDexBaseProvider().SearchAdvanced("", types.TypeManga, []types.Format{types.FormatManga}, 0, 25, []string{"Aliens"}, []string{"Harem"}, types.SeasonUnknown, 0, []string{"Isekai"}, []string{"Harem"})
+		if err != nil {
+			panic(err)
+		}
+
+		for _, r := range res {
+			// print r.title.english
+			println(*r.Title.English)
+		}
+	*/
+
+	/*
+		res, err := base.NewMangaDexBaseProvider().GetMedia("ed5dddc0-999d-4fd2-a702-9fef1ec2c955")
+		if err != nil {
+			panic(err)
+		}
+
+		println(*res.Title.English)
+	*/
+
+	/*
+		res, err := base.NewMangaDexBaseProvider().GetSeasonal(types.TypeAnime, []types.Format{types.FormatManga})
+		if err != nil {
+			panic(err)
+		}
+
+		println("Popular:")
+
+		for _, r := range res.Popular {
+			println(*r.Title.English)
+		}
+
+		println("Seasonal:")
+
+		for _, r := range res.Seasonal {
+			println(*r.Title.English)
+		}
+
+		println("Top:")
+
+		for _, r := range res.Top {
+			println(*r.Title.English)
+		}
+
+		println("Trending:")
+
+		for _, r := range res.Trending {
+			println(*r.Title.English)
+		}
+	*/
 
 	/*
 		resp, err := request.Request("novelupdates", false, http.Request{
--- src/mappings/impl/base/mangadex.go
+++ src/mappings/impl/base/mangadex.go
@@ -10,6 +10,7 @@ import (
 	"net/url"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type MangaDexBaseProvider struct {
@@ -335,7 +336,305 @@ func (p *MangaDexBaseProvider) SearchAdvanced(query string, mediaType types.Type
 	return results, nil
 }
 
-func extractTitle(attributes Attributes) types.Title {
+func (p *MangaDexBaseProvider) GetCurrentSeason() (types.Season, error) {
+	return types.SeasonUnknown, nil
+}
+
+func (p *MangaDexBaseProvider) GetMedia(id string) (types.MediaInfo, error) {
+	uri, _ := url.Parse(p.Api + "/manga/" + id)
+
+	data, err := p.Request(http.Request{
+		URL:    uri,
+		Method: "GET",
+	}, &p.NeedsProxy)
+	if err != nil {
+		return types.MediaInfo{}, err
+	}
+	defer data.Response.Body.Close()
+
+	if data.Response.StatusCode != 200 {
+		return types.MediaInfo{}, fmt.Errorf("unexpected status code: %d", data.Response.StatusCode)
+	}
+
+	if data.Response.Header.Get("Content-Type") != "application/json" {
+		return types.MediaInfo{}, fmt.Errorf("invalid content type: %s", data.Response.Header.Get("Content-Type"))
+	}
+
+	body, err := io.ReadAll(data.Response.Body)
+	if err != nil {
+		return types.MediaInfo{}, fmt.Errorf("error reading response body: %w", err)
+	}
+
+	var itemData MangaDexItem
+	if err := json.Unmarshal(body, &itemData); err != nil {
+		fmt.Printf("JSON parsing error: %v\nResponse: %s\n", err, string(body))
+		return types.MediaInfo{}, fmt.Errorf("error parsing JSON: %w", err)
+	}
+
+	altTitles := extractAltTitles(itemData.Data.Attributes)
+	img := extractCoverArtURL(itemData.Data.Relationships, p.Api, id)
+	format := determineFormat(itemData.Data.Type)
+	title := extractTitle(itemData.Data.Attributes)
+	genres := extractGenres(itemData.Data.Attributes.Tags)
+	description := extractDescription(itemData.Data.Attributes.Description)
+	countryOfOrigin := extractCountryOfOrigin(itemData.Data.Attributes)
+	tags := extractTags(itemData.Data.Attributes.Tags)
+	author := extractAuthor(itemData.Data.Relationships)
+	publisher := extractPublisher(itemData.Data.Relationships)
+
+	return types.MediaInfo{
+		ID:              id,
+		Title:           title,
+		Artwork:         nil,
+		Synonyms:        altTitles,
+		TotalChapters:   helper.ConvertStringToIntPointer(itemData.Data.Attributes.LastChapter),
+		BannerImage:     nil,
+		CoverImage:      &img,
+		Color:           nil,
+		Year:            &itemData.Data.Attributes.Year,
+		Status:          &itemData.Data.Attributes.Status,
+		Genres:          genres,
+		Description:     &description,
+		Format:          format,
+		TotalVolumes:    helper.ConvertStringToIntPointer(itemData.Data.Attributes.LastVolume),
+		CountryOfOrigin: &countryOfOrigin,
+		Tags:            tags,
+		Relations:       nil,
+		Characters:      nil,
+		TotalEpisodes:   nil,
+		CurrentEpisode:  nil,
+		Season:          types.SeasonUnknown,
+		Duration:        nil,
+		Trailer:         nil,
+		Type:            types.TypeManga,
+		Rating:          nil,
+		Popularity:      nil,
+		Author:          &author,
+		Publisher:       &publisher,
+	}, nil
+}
+
+func (p *MangaDexBaseProvider) GetSeasonal(mediaType types.Type, formats []types.Format) (types.SeasonalResponse, error) {
+	currentDate := time.Now().AddDate(0, 0, -3)
+	createdAtParam := fmt.Sprintf("%04d-%02d-%02dT00:00:00",
+		currentDate.Year(),
+		int(currentDate.Month()),
+		currentDate.Day(),
+	)
+
+	trendingUri, _ := url.Parse(p.Api + "/manga?includes[]=cover_art&includes[]=artist&includes[]=author&order[followedCount]=desc&contentRating[]=safe&contentRating[]=suggestive&hasAvailableChapters=true&createdAtSince=" + createdAtParam)
+	popularUri, _ := url.Parse(p.Api + "/manga?includes[]=cover_art&includes[]=artist&includes[]=author&order[followedCount]=desc&contentRating[]=safe&contentRating[]=suggestive&hasAvailableChapters=true")
+	topUri, _ := url.Parse(p.Api + "/manga?includes[]=cover_art&includes[]=artist&includes[]=author&order[rating]=desc&contentRating[]=safe&contentRating[]=suggestive&hasAvailableChapters=true")
+	// https://mangadex.org/titles/seasonal
+	seasonalUri, _ := url.Parse(p.Api + "/list/54736a5c-eb7f-4844-971b-80ee171cdf29?includes[]=user")
+
+	trending, err := p.Request(http.Request{
+		URL:    trendingUri,
+		Method: "GET",
+	}, &p.NeedsProxy)
+	if err != nil {
+		return types.SeasonalResponse{}, err
+	}
+	defer trending.Response.Body.Close()
+
+	if trending.Response.StatusCode != 200 {
+		return types.SeasonalResponse{}, fmt.Errorf("unexpected status code: %d", trending.Response.StatusCode)
+	}
+
+	if trending.Response.Header.Get("Content-Type") != "application/json" {
+		return types.SeasonalResponse{}, fmt.Errorf("invalid content type: %s", trending.Response.Header.Get("Content-Type"))
+	}
+
+	trendingBody, err := io.ReadAll(trending.Response.Body)
+	if err != nil {
+		return types.SeasonalResponse{}, fmt.Errorf("error reading response body: %w", err)
+	}
+
+	var trendingData MangaDexSeasonal
+	if err := json.Unmarshal(trendingBody, &trendingData); err != nil {
+		fmt.Printf("JSON parsing error: %v\nResponse: %s\n", err, string(trendingBody))
+		return types.SeasonalResponse{}, fmt.Errorf("error parsing JSON: %w", err)
+	}
+
+	popular, err := p.Request(http.Request{
+		URL:    popularUri,
+		Method: "GET",
+	}, &p.NeedsProxy)
+	if err != nil {
+		return types.SeasonalResponse{}, err
+	}
+	defer popular.Response.Body.Close()
+
+	if popular.Response.StatusCode != 200 {
+		return types.SeasonalResponse{}, fmt.Errorf("unexpected status code: %d", popular.Response.StatusCode)
+	}
+
+	if popular.Response.Header.Get("Content-Type") != "application/json" {
+		return types.SeasonalResponse{}, fmt.Errorf("invalid content type: %s", popular.Response.Header.Get("Content-Type"))
+	}
+
+	popularBody, err := io.ReadAll(popular.Response.Body)
+	if err != nil {
+		return types.SeasonalResponse{}, fmt.Errorf("error reading response body: %w", err)
+	}
+
+	var popularData MangaDexSeasonal
+	if err := json.Unmarshal(popularBody, &popularData); err != nil {
+		fmt.Printf("JSON parsing error: %v\nResponse: %s\n", err, string(popularBody))
+		return types.SeasonalResponse{}, fmt.Errorf("error parsing JSON: %w", err)
+	}
+
+	top, err := p.Request(http.Request{
+		URL:    topUri,
+		Method: "GET",
+	}, &p.NeedsProxy)
+	if err != nil {
+		return types.SeasonalResponse{}, err
+	}
+	defer top.Response.Body.Close()
+
+	if top.Response.StatusCode != 200 {
+		return types.SeasonalResponse{}, fmt.Errorf("unexpected status code: %d", top.Response.StatusCode)
+	}
+
+	if top.Response.Header.Get("Content-Type") != "application/json" {
+		return types.SeasonalResponse{}, fmt.Errorf("invalid content type: %s", top.Response.Header.Get("Content-Type"))
+	}
+
+	topBody, err := io.ReadAll(top.Response.Body)
+	if err != nil {
+		return types.SeasonalResponse{}, fmt.Errorf("error reading response body: %w", err)
+	}
+
+	var topData MangaDexSeasonal
+	if err := json.Unmarshal(topBody, &topData); err != nil {
+		fmt.Printf("JSON parsing error: %v\nResponse: %s\n", err, string(topBody))
+		return types.SeasonalResponse{}, fmt.Errorf("error parsing JSON: %w", err)
+	}
+
+	seasonal, err := p.Request(http.Request{
+		URL:    seasonalUri,
+		Method: "GET",
+	}, &p.NeedsProxy)
+	if err != nil {
+		return types.SeasonalResponse{}, err
+	}
+	defer seasonal.Response.Body.Close()
+
+	if seasonal.Response.StatusCode != 200 {
+		return types.SeasonalResponse{}, fmt.Errorf("unexpected status code: %d", seasonal.Response.StatusCode)
+	}
+
+	if seasonal.Response.Header.Get("Content-Type") != "application/json" {
+		return types.SeasonalResponse{}, fmt.Errorf("invalid content type: %s", seasonal.Response.Header.Get("Content-Type"))
+	}
+
+	seasonalBody, err := io.ReadAll(seasonal.Response.Body)
+	if err != nil {
+		return types.SeasonalResponse{}, fmt.Errorf("error reading response body: %w", err)
+	}
+
+	var seasonalData MangaDexSeasonal
+	if err := json.Unmarshal(topBody, &seasonalData); err != nil {
+		fmt.Printf("JSON parsing error: %v\nResponse: %s\n", err, string(seasonalBody))
+		return types.SeasonalResponse{}, fmt.Errorf("error parsing JSON: %w", err)
+	}
+
+	return types.SeasonalResponse{
+		Trending: returnFilledManga(p, trendingData),
+		Popular:  returnFilledManga(p, popularData),
+		Top:      returnFilledManga(p, topData),
+		Seasonal: returnFilledManga(p, seasonalData),
+	}, nil
+}
+
+func (p *MangaDexBaseProvider) GetSchedule() (types.ScheduleResponse, error) {
+	return types.ScheduleResponse{}, nil
+}
+
+func (p *MangaDexBaseProvider) GetIds() ([]string, error) {
+	uri, err := url.Parse("https://raw.githubusercontent.com/ArdaxHz/mangadex-id-map/main/json/manga_map.json")
+	if err != nil {
+		return nil, fmt.Errorf("error parsing URL: %w", err)
+	}
+
+	ids, err := p.Request(http.Request{
+		URL:    uri,
+		Method: "GET",
+	}, &p.NeedsProxy)
+	if err != nil {
+		return nil, err
+	}
+	defer ids.Response.Body.Close()
+
+	if ids.Response.StatusCode != 200 {
+		return nil, fmt.Errorf("unexpected status code: %d", ids.Response.StatusCode)
+	}
+
+	idsBody, err := io.ReadAll(ids.Response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading response body: %w", err)
+	}
+
+	var idsData map[string]string
+	if err := json.Unmarshal(idsBody, &idsData); err != nil {
+		fmt.Printf("JSON parsing error: %v\nResponse: %s\n", err, string(idsBody))
+		return nil, fmt.Errorf("error parsing JSON: %w", err)
+	}
+
+	var idList []string
+	for id := range idsData {
+		idList = append(idList, id)
+	}
+
+	return idList, nil
+}
+
+func returnFilledManga(p *MangaDexBaseProvider, seasonalData MangaDexSeasonal) []types.MediaInfo {
+	var results []types.MediaInfo
+	for _, data := range seasonalData.Data {
+		altTitles := extractAltTitles(data.Attributes)
+		img := extractCoverArtURL(data.Relationships, p.Api, data.ID)
+		format := determineFormat(data.Type)
+		title := extractTitle(data.Attributes)
+		genres := extractGenres(data.Attributes.Tags)
+		description := extractDescription(data.Attributes.Description)
+		countryOfOrigin := extractCountryOfOrigin(data.Attributes)
+		tags := extractTags(data.Attributes.Tags)
+		author := extractAuthor(data.Relationships)
+		publisher := extractPublisher(data.Relationships)
+
+		results = append(results, types.MediaInfo{
+			ID:              data.ID,
+			Title:           title,
+			Artwork:         nil,
+			Synonyms:        altTitles,
+			TotalChapters:   helper.ConvertStringToIntPointer(data.Attributes.LastChapter),
+			BannerImage:     nil,
+			CoverImage:      &img,
+			Color:           nil,
+			Year:            &data.Attributes.Year,
+			Status:          &data.Attributes.Status,
+			Genres:          genres,
+			Description:     &description,
+			Format:          format,
+			TotalVolumes:    helper.ConvertStringToIntPointer(data.Attributes.LastVolume),
+			CountryOfOrigin: &countryOfOrigin,
+			Tags:            tags,
+			Relations:       nil,
+			Characters:      nil,
+			Author:          &author,
+			Publisher:       &publisher,
+			Type:            types.TypeManga,
+			Rating:          nil,
+			Popularity:      nil,
+		})
+	}
+
+	return results
+}
+
+func extractTitle(attributes ItemAttributes) types.Title {
 	title := types.Title{
 		English: findTitle(attributes, "en"),
 		Romaji:  findRomajiTitle(attributes),
@@ -344,7 +643,7 @@ func extractTitle(attributes Attributes) types.Title {
 	return title
 }
 
-func findTitle(attributes Attributes, lang string) *string {
+func findTitle(attributes ItemAttributes, lang string) *string {
 	for _, altTitle := range attributes.AltTitles {
 		if val, exists := altTitle[lang]; exists {
 			return &val
@@ -357,7 +656,7 @@ func findTitle(attributes Attributes, lang string) *string {
 	return nil
 }
 
-func findRomajiTitle(attributes Attributes) *string {
+func findRomajiTitle(attributes ItemAttributes) *string {
 	if val, exists := attributes.Title["ja-ro"]; exists {
 		return &val
 	}
@@ -376,7 +675,7 @@ func findRomajiTitle(attributes Attributes) *string {
 	return nil
 }
 
-func findNativeTitle(attributes Attributes) *string {
+func findNativeTitle(attributes ItemAttributes) *string {
 	if val, exists := attributes.Title["jp"]; exists {
 		return &val
 	}
@@ -401,7 +700,7 @@ func findNativeTitle(attributes Attributes) *string {
 	return nil
 }
 
-func extractAltTitles(attributes Attributes) []string {
+func extractAltTitles(attributes ItemAttributes) []string {
 	var altTitles []string
 	for _, titleMap := range attributes.AltTitles {
 		for _, title := range titleMap {
@@ -448,7 +747,7 @@ func extractDescription(description map[string]string) string {
 	}
 }
 
-func extractCountryOfOrigin(attributes Attributes) string {
+func extractCountryOfOrigin(attributes ItemAttributes) string {
 	if attributes.PublicationDemographic != "" {
 		return attributes.PublicationDemographic
 	} else {
@@ -504,22 +803,32 @@ type MangaDexSearch struct {
 
 type SearchResult struct {
 	ID            string         `json:"id"`
-	Attributes    Attributes     `json:"attributes"`
+	Attributes    ItemAttributes `json:"attributes"`
 	Relationships []Relationship `json:"relationships"`
 	Type          string         `json:"type"`
 }
 
-type Attributes struct {
-	Description            map[string]string   `json:"description"`
-	AltTitles              []map[string]string `json:"altTitles"`
-	Title                  map[string]string   `json:"title"`
-	Year                   int                 `json:"year"`
-	LastChapter            string              `json:"lastChapter"`
-	LastVolume             string              `json:"lastVolume"`
-	PublicationDemographic string              `json:"publicationDemographic"`
-	OriginalLanguage       string              `json:"originalLanguage"`
-	Status                 string              `json:"status"`
-	Tags                   []Tag               `json:"tags"`
+type ItemAttributes struct {
+	Title                          map[string]string   `json:"title"`
+	AltTitles                      []map[string]string `json:"altTitles"`
+	Description                    map[string]string   `json:"description"`
+	IsLocked                       bool                `json:"isLocked"`
+	Links                          map[string]string   `json:"links"`
+	OriginalLanguage               string              `json:"originalLanguage"`
+	LastVolume                     string              `json:"lastVolume"`
+	LastChapter                    string              `json:"lastChapter"`
+	PublicationDemographic         string              `json:"publicationDemographic"`
+	Status                         string              `json:"status"`
+	Year                           int                 `json:"year"`
+	ContentRating                  string              `json:"contentRating"`
+	Tags                           []Tag               `json:"tags"`
+	State                          string              `json:"state"`
+	ChapterNumbersResetOnNewVolume bool                `json:"chapterNumbersResetOnNewVolume"`
+	CreatedAt                      string              `json:"createdAt"`
+	UpdatedAt                      string              `json:"updatedAt"`
+	Version                        int                 `json:"version"`
+	AvailableTranslatedLanguages   []string            `json:"availableTranslatedLanguages"`
+	LatestUploadedChapter          string              `json:"latestUploadedChapter"`
 }
 
 type Relationship struct {
@@ -565,4 +874,23 @@ type TagGroupAttributes struct {
 	Description map[string]string `json:"description"`
 	Group       string            `json:"group"`
 	Version     int               `json:"version"`
+}
+
+type MangaDexItem struct {
+	Result   string   `json:"result"`
+	Response string   `json:"response"`
+	Data     ItemData `json:"data"`
+}
+
+type ItemData struct {
+	ID            string         `json:"id"`
+	Type          string         `json:"type"`
+	Attributes    ItemAttributes `json:"attributes"`
+	Relationships []Relationship `json:"relationships"`
+}
+
+type MangaDexSeasonal struct {
+	Result   string     `json:"result"`
+	Response string     `json:"response"`
+	Data     []ItemData `json:"data"`
 }
